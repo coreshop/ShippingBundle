2f62abb65b24a796edbe7d4b088e9c7e64388223
dpfaffenbauer <5981845+dpfaffenbauer@users.noreply.github.com>
2026-06-10 14:12:43 +0000

    Build CoreShop Studio bundles [skip ci]

--- Resources/public/studio/90c4d12e-cd92-497f-bc1b-64e9b43071a8/exposeRemote.js
+++ Resources/public/studio/90c4d12e-cd92-497f-bc1b-64e9b43071a8/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.coreshopshipping = "/bundles/coreshopshipping/studio/560e63ba-87dc-4de5-a8a8-e6ca05e141d2/static/js/remoteEntry.js"
+      window.pluginRemotes.coreshopshipping = "/bundles/coreshopshipping/studio/90c4d12e-cd92-497f-bc1b-64e9b43071a8/static/js/remoteEntry.js"
 
       
     
